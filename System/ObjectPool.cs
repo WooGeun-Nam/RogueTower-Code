85190b26b913786a5629889bb13610367e659715
WooGeun-Nam <82949734+WooGeun-Nam@users.noreply.github.com>
2025-10-03 02:09:36 +0900

    Mod ObjectPool.cs

--- System/ObjectPool.cs
+++ System/ObjectPool.cs
@@ -1,9 +1,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// [추가] 오브젝트 풀링 시스템
-// 기능: 게임 오브젝트의 생성과 파괴를 최소화하여 성능을 최적화
-// 삭제된 내용: 없음
+// 오브젝트 풀링 시스템
+// 게임 오브젝트의 생성과 파괴를 최소화하여 성능을 최적화
 public class ObjectPool<T> where T : UnityEngine.Object
 {
     private readonly Stack<T> _pool = new Stack<T>();
@@ -18,7 +17,7 @@ public class ObjectPool<T> where T : UnityEngine.Object
         for (int i = 0; i < initialSize; i++)
         {
             T obj = Object.Instantiate(prefab, parent);
-            // [수정] T가 GameObject 또는 Component일 경우에만 SetActive 호출
+            // T가 GameObject 또는 Component일 경우에만 SetActive 호출
             if (obj is GameObject go) go.SetActive(false);
             else if (obj is Component comp) comp.gameObject.SetActive(false);
             _pool.Push(obj);
@@ -31,16 +30,16 @@ public class ObjectPool<T> where T : UnityEngine.Object
         while (_pool.Count > 0)
         {
             obj = _pool.Pop();
-            if (obj) // 유효성 검사 수정
+            if (obj) // 유효성 검사
             {
-                // [수정] T가 GameObject 또는 Component일 경우에만 SetActive 호출
+                // T가 GameObject 또는 Component일 경우에만 SetActive 호출
                 if (obj is GameObject go1) go1.SetActive(true);
                 else if (obj is Component comp1) comp1.gameObject.SetActive(true);
                 return obj;
             }
         }
         obj = Object.Instantiate(_prefab, _parent);
-        // [수정] T가 GameObject 또는 Component일 경우에만 SetActive 호출
+        // T가 GameObject 또는 Component일 경우에만 SetActive 호출
         if (obj is GameObject go2) go2.SetActive(true);
         else if (obj is Component comp2) comp2.gameObject.SetActive(true);
         return obj;
@@ -48,9 +47,9 @@ public class ObjectPool<T> where T : UnityEngine.Object
 
     public void Return(T obj)
     {
-        if (obj) // 유효성 검사 추가
+        if (obj) // 유효성 검사
         {
-            // [수정] T가 GameObject 또는 Component일 경우에만 SetActive 호출
+            // T가 GameObject 또는 Component일 경우에만 SetActive 호출
             if (obj is GameObject go) go.SetActive(false);
             else if (obj is Component comp) comp.gameObject.SetActive(false);
             _pool.Push(obj);
